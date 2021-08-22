7b220c290f58a244ac102055793841cf2495ec63
nhamilton1 <nickh3592@gmail.com>
2021-08-22 08:26:57 -0400

    having issues with my firebase api key

--- potluck/src/components/App.js
+++ potluck/src/components/App.js
@@ -1,34 +1,50 @@
-import React from "react"
-import Signup from "./Signup"
-import { Container } from "react-bootstrap"
-import { AuthProvider } from "../contexts/AuthContext"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Dashboard from "./Dashboard"
-
-
-
+import React from 'react'
+import Signup from './Signup'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { AuthProvider } from '../context/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Dashboard from './Dashboard'
+import Login from './Login'
+import PrivateRoute from './PrivateRoute'
+import ForgotPassword from './ForgotPassword';
+import UpdateProfile from './UpdateProfile';
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-            </Switch>
-          </AuthProvider>
-        </Router>
-      </div>
-    </Container>
+    <>
+    <Navbar bg="primary" variant="dark">
+      <Container>
+      <Navbar.Brand href="/">PotLuck</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="/create-an-event">Create an Event</Nav.Link>
+        <Nav.Link href="/food-list">Food List</Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link href="/user-dashboard">Dashboard</Nav.Link>
+      </Nav>
+      </Container>
+    </Navbar>
+      <Container 
+        className='d-flex align-items-center justify-content-center'
+        style={{minHeight: '100vh'}}
+      >
+        <div className='w-100' style={{ maxWidth: '400px' }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path ='/user-dashboard' component={Dashboard} />
+                <PrivateRoute path ='/update-profile' component={UpdateProfile} />
+                <Route path='/signup' component={Signup} />
+                <Route path='/login' component={Login} />
+                <Route path='/forgot-password' component={ForgotPassword} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+      </>
   )
 }
 
-export default App
+export default App;
--- potluck/src/context/AuthContext.js
+++ potluck/src/context/AuthContext.js
@@ -1,62 +1,69 @@
-import React, { useContext, useState, useEffect } from "react"
-import { auth } from "../firebase"
+import React, { useContext, useState, useEffect } from 'react'
+import { auth } from '../firebase'
 
 const AuthContext = React.createContext()
 
 export function useAuth() {
-  return useContext(AuthContext)
+    return useContext(AuthContext)
 }
 
+
 export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState()
-  const [loading, setLoading] = useState(true)
+    const [ currentUser, setCurrentUser ] = useState()
+    const [ loading, setLoading ] = useState(true)
 
-  function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password)
-  }
 
-  function login(email, password) {
-    return auth.signInWithEmailAndPassword(email, password)
-  }
+    function signup (email, password) {
+       return auth.createUserWithEmailAndPassword(email, password)
+    }
+    // IF NOT USING FIREBASE CHANGE THE TOP LINE AND BOTTOM LINES OF CODE, REST OF APPLICATION WILL WORK FINE
+    function login (email, password) {
+        return auth.signInWithEmailAndPassword(email,password)
+    }
 
-  function logout() {
-    return auth.signOut()
-  }
+    function logout () {
+        return auth.signOut
+    }
 
-  function resetPassword(email) {
-    return auth.sendPasswordResetEmail(email)
-  }
+    function resetPassword (email) {
+        return auth.sendPasswordResetEmail(email)
+    }
 
-  function updateEmail(email) {
-    return currentUser.updateEmail(email)
-  }
+    function updateEmail (email) {
+        return currentUser.updateEmail(email)
+    }
 
-  function updatePassword(password) {
-    return currentUser.updatePassword(password)
-  }
+    function updatePassword (password) {
+        return currentUser.updatePassword(password)
+    }
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      setCurrentUser(user)
-      setLoading(false)
-    })
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+          setCurrentUser(user)
+          setLoading(false)
+        })
+    
+        return unsubscribe
+      }, [])
 
-    return unsubscribe
-  }, [])
 
-  const value = {
-    currentUser,
-    login,
-    signup,
-    logout,
-    resetPassword,
-    updateEmail,
-    updatePassword
-  }
 
-  return (
-    <AuthContext.Provider value={value}>
-      {!loading && children}
-    </AuthContext.Provider>
-  )
+    const value = {
+        currentUser,
+        login,
+        signup,
+        logout,
+        resetPassword,
+        updateEmail,
+        updatePassword
+        //here we are exposing all of our functions to all of our different components
+    }
+
+    
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
 }
