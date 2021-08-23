1297fc9c9c9198135f598ba14e68c02af082a8fc
Allison Homem <mrs.allisonhomem@outlook.com>
2021-08-23 17:13:48 -0500

    testing out using react-bootstrap to create form

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -1,10 +1,42 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
+import { Container, Form } from 'react-bootstrap';
+
+
+//schema
+
 
 export default function CreateAnEvent() {
+    //set up state
+
+    //on change event hangler
+
+    //validate schema function
+
+    //on submit event handler
+
+
     return (
         <Container>
-           Create an Event Test
+           {/* make a form */}
+           <Form>
+
+           {/* title input */}
+             <Form.Group>
+              <Form.Label>Event Name</Form.Label>
+              <Form.Control type="text" placeholder="enter the name of your party here" />
+             </Form.Group>
+
+           {/* description input */}
+
+           {/* date input: month, day, year */}
+
+           {/* time input */}
+
+           {/* location input */}
+           
+           
+           </Form>
+
         </Container>
     )
 }
