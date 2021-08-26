a02e2b436c08b47a9fda2cdb4289f2566a4a5eeb
courtneyharris <harriscourtney98@gmail.com>
2021-08-26 16:24:45 -0400

    Started working on a means of submitting everything. Could probably be refined a bit more

--- src/components/Events/FoodList.js
+++ src/components/Events/FoodList.js
@@ -2,20 +2,91 @@
 import FromPropertyDescriptor from 'es-abstract/5/FromPropertyDescriptor';
 import { Card, Container, Form, Button } from 'react-bootstrap';
 import './FoodList.css'
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+import axios from 'axios';
+
+
+
+const emptyList = {
+    item1: '',
+    item2: '',
+    item3: '',
+    item4: '',
+    item5: '',
+    description: '',
+}
+
+const schema = yup.object().shape({
+    item1: yup
+        .boolean(),
+    item2: yup
+        .boolean(),
+    item3: yup
+        .boolean(),
+    item4: yup
+        .boolean(),
+    item5: yup
+        .boolean(),
+    description: yup
+        .string()
+        .min(3, 'More characters are needed')
+        .max(120, 'Max character limit reached')
+});
+
+export default function FoodList() {
+
+
+    const [list, setList] = useState(emptyList);
+    const [errors, setErrors] = useState(emptyList);
+    const [events, setEvents] = useState([]);
+    const [disabled, setDisabled] = useState();
+
+    const setListErrors = (name, value) => {
+        yup.reach(schema, name).validate(value)
+            .then(() => setErrors({ ...errors, [name]: '' }))
+            .catch(err => setErrors({ ...errors, [name]: err.errors[0] }))
+    }
+
+    const change = event => {
+        const { value, name } = event.target;
+        setListErrors(name, value);
+        setList({ ...list, [name]: value });
+    }
+
+    useEffect(() => {
+        schema.isValid(list)
+            .then(valid => setDisabled(!valid))
+    }, [list]);
 
 
 
 
 
-export default function FoodList(props) {
 
-    const { values, update, submit } = props;
+    // const { values, update, submit } = props;
 
     const onSubmit = evt => {
 
         evt.preventDefault();
 
-        submit();
+        const newEvent = {
+            item1: list.item1,
+            item2: list.item2,
+            item3: list.item3,
+            item4: list.item4,
+            item5: list.item5,
+            description: list.description,
+        }
+
+        axios.post(``, newEvent)
+            .then(res => {
+                setEvents([res.data, ...events])
+                setList(emptyList)
+            })
+            .catch(err => {
+                console.error('jeez this is embarassing, looks like there is an error')
+            })
     }
 
 
@@ -36,23 +107,29 @@ export default function FoodList(props) {
                         <div className="rowOne">
                             <label> Appetizers
                                 <input
-                                    type="radio"
-                                    name="appetizers"
+                                    type="checkbox"
+                                    name="item1"
+                                    value={list.item1}
+                                    onChange={change}
                                 />
                             </label>
 
                             <label> Entrees
                                 <input
-                                    type="radio"
-                                    name="entrees"
+                                    type="checkbox"
+                                    name="item2"
+                                    value={list.item2}
+                                    onChange={change}
                                 />
                             </label>
 
 
                             <label> Dessert
                                 <input
-                                    type="radio"
-                                    name="dessert"
+                                    type="checkbox"
+                                    name="item3"
+                                    value={list.item3}
+                                    onChange={change}
                                 />
                             </label>
                         </div>
@@ -60,16 +137,20 @@ export default function FoodList(props) {
                         <div className="rowTwo">
                             <label> Non-Alcoholic Drinks
                                 <input
-                                    type="radio"
-                                    name="drinks"
+                                    type="checkbox"
+                                    name="item4"
+                                    value={list.item4}
+                                    onChange={change}
                                 />
                             </label>
 
 
                             <label> Alcoholic Drinks
                                 <input
-                                    type="radio"
-                                    name="alcohol"
+                                    type="checkbox"
+                                    name="item5"
+                                    value={list.item5}
+                                    onChange={change}
                                 />
                             </label>
                         </div>
@@ -81,13 +162,22 @@ export default function FoodList(props) {
                         <Card.Title>
                             <Card.Text style={{ fontsize: '25px', }}>Specify what you are bringing</Card.Text>
                         </Card.Title>
-                        <Form.Control type="text" placeholer="Enter dish here" />
+                        <Form.Control
+                            type="text"
+                            name="description"
+                            value={list.description}
+                            onChange={change}
+                            placeholder="Enter dish here" />
                     </Form.Group>
 
 
                     <Form.Group>
 
-                        <Button variant="primary" type="submit" style={{justifyContent: 'center', marginTop: '30px'}}>Submit</Button>
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            disabled={disabled}
+                            style={{ justifyContent: 'center', marginTop: '30px' }}>Submit</Button>
                     </Form.Group>
 
                 </Form>
